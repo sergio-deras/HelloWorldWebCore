98f50a7c214097c88969403553f18cf612ec0f4a
Sergio Deras <sergio.deras@gmail.com>
2020-06-15 15:58:39 -0500

    Fix path

--- installApp.sh
+++ installApp.sh
@@ -1,3 +1,3 @@
 
 sudo killall -9 dotnet
-nohup dotnet /home/ec2-user/src/HelloWorldWebCore/build_output/HelloWorldWebCore.dll &>/dev/null &
+nohup dotnet /home/ec2-user/build_output/HelloWorldWebCore.dll &
